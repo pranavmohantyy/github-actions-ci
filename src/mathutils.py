1c1c17db4f2868e94ba65c93abecb4b8838753b5
pranavmohantyy <mohantypranav147@gmail.com>
2026-08-25 19:23:12 -0700

    add type checking with mypy

--- src/mathutils.py
+++ src/mathutils.py
@@ -1,13 +1,13 @@
-def add(x, y):
+def add(x: float, y: float) -> float:
     return x + y
 
-def subtract(x, y):
+def subtract(x: float, y: float) -> float:
     return x - y
 
-def multiply(x, y):
+def multiply(x: float, y: float) -> float:
     return x * y
 
-def divide(x, y):
+def divide(x: float, y: float) -> float:
     if y == 0:
         raise ValueError("Cannot divide by zero")
     return x / y
